5c8a751ded9d90ef1fad14e87733723ea95e01df
Bernard <berni.kastrati@hotmail.com>
2021-10-03 11:35:56 +0200

    my change

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-// alert("This is website");
-
-// Toggle Animation by Class
-// $(window).scroll(function(){
-//     if($(document).scrollTop() > 100){
-//       $('nav').addClass('affix');
-//     }else{
-//       $('nav').removeClass('affix+');
-//     }
-//   })
-
-
-//   $(window).scroll(function() {
-//     if ($(document).scrollTop() > 50) {
-//         $('nav').addClass('affix');
-//         console.log("OK");
-//     } else {
-//         $('nav').removeClass('affix');
-//     }
-// });
 
 
 function button(){
@@ -26,3 +6,32 @@ function button(){
 
 
 
+// NAV
+
+
+const navSlide = () => {
+    const phone = document.querySelector('.transform-phone');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li')
+
+    phone.addEventListener('click', () => {
+        // Toggle Now
+        nav.classList.toggle('nav-active');
+
+         //animate links
+        navLinks.forEach((link, index) => {
+            if(link.style.animation){
+                link.style.animation = '';
+            } else{
+                link.style.animation = `navLinkFade 0.5 ease forwards ${index / 7 + 1.5}s`;
+            }
+        });
+        
+          //ikon animation
+        phone.classList.toggle('toggle');
+    });
+  
+    
+}
+
+navSlide();
